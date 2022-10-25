85c879e726521f01cb285246a71d76da2bbd847f
Anouar-91 <a.berrouane@hotmail.com>
2022-10-25 17:37:00 +0200

    handle errors

--- src/server.js
+++ src/server.js
@@ -2,12 +2,11 @@ import express  from 'express';
 import dotenv from "dotenv";
 import connectDb from './config/database';
 import productRoutes from './routes/productRoutes';
+import {notFound, errorHandler} from './middleware/errorMiddleware'
 
-const app = express();
 dotenv.config();
 connectDb();
-
-
+const app = express();
 let cors = require("cors");
 app.use(cors());
 
@@ -16,6 +15,12 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/products', productRoutes);
+
+//middleware not found
+app.use(notFound)
+
+//middleware errors
+app.use(errorHandler)
 
 const PORT = process.env.PORT;
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port `+ PORT ))

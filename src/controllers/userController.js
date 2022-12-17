d4c88d2e93febdf42823989579e1c6f51c8df67e
Anouar-91 <a.berrouane@hotmail.com>
2022-12-17 20:42:35 +0100

    update profile

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -37,6 +37,33 @@ const getUserProfile = asyncHandler(async (req, res) => {
             isAdmin: user.isAdmin, 
         })
     }else{
+        res.status(404)
+        throw new Error("User not found")
+    }
+})
+
+// @desc update user profile
+// @route PUT /api/users/profile
+// @access Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id)
+    console.log(user)
+    if(user){
+        user.name = req.body.name || user.name
+        user.email = req.body.email || user.email
+        if(req.body.password){
+            user.password = req.body.password
+        }
+        const updateUser = await user.save()
+        res.json({
+            _id: updateUser._id,
+            name: updateUser.name,
+            email: updateUser.email, 
+            isAdmin: updateUser.isAdmin, 
+            token: generateToken(updateUser._id)
+        })
+    }else{
+        res.status(404)
         throw new Error("User not found")
     }
 })
@@ -77,5 +104,6 @@ const registerUser = asyncHandler(async (req, res) => {
 export {
     authUser,
     getUserProfile,
-    registerUser
+    registerUser,
+    updateUserProfile
 }
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
 const router = express.Router();
-import {authUser, getUserProfile, registerUser} from '../controllers/userController';
+import {authUser, getUserProfile, registerUser, updateUserProfile} from '../controllers/userController';
 import {protect} from '../middleware/authMiddleware';
 
 
 router.route('/login').post(authUser)
 router.route('/profile').get(protect, getUserProfile)
+router.route('/profile').put(protect, updateUserProfile)
 router.route('/register').post( registerUser)
 
 export default router;

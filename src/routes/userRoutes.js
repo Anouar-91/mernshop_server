78fca9106397c0120ca1dff50db35ed8cfa38954
Anouar-91 <a.berrouane@hotmail.com>
2023-01-18 17:38:10 +0100

    add getUserById and updateUserById

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 const router = express.Router();
-import {authUser, deleteUser, getUserProfile, getUsers, registerUser, updateUserProfile} from '../controllers/userController';
+import {authUser, deleteUser, getUserById, getUserProfile, getUsers, registerUser, updateUser, updateUserProfile} from '../controllers/userController';
 import {protect, admin} from '../middleware/authMiddleware';
 
 
@@ -10,5 +10,7 @@ router.route('/profile').get(protect, getUserProfile)
 router.route('/profile').put(protect, updateUserProfile)
 router.route('/register').post( registerUser)
 router.route('/:id').delete(protect, admin, deleteUser)
+router.route('/:id').get(protect, admin, getUserById)
+router.route('/:id').put(protect, admin, updateUser)
 
 export default router;

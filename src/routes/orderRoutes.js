d25688c585f594cbc220ef9f73727a3e258bff28
Anouar-91 <a.berrouane@hotmail.com>
2023-01-23 21:07:01 +0100

    list orders

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 const router = express.Router();
-import {addOrderItems, getMyOrders, getOrderById, updateOrderToPaid} from '../controllers/orderController';
-import {protect} from '../middleware/authMiddleware.js';
+import {addOrderItems, getMyOrders, getOrderById, getOrders, updateOrderToPaid} from '../controllers/orderController';
+import {protect, admin} from '../middleware/authMiddleware.js';
 
 router.route('/').post(protect, addOrderItems);
+router.route('/').get(protect,admin, getOrders);
 router.route('/myorders').get(protect, getMyOrders);
 router.route('/:id').get(protect, getOrderById);
 router.route('/:id/pay').put(protect, updateOrderToPaid);
